a8e12d19dfcc655027791c0367732de69b28856c
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-03-05 16:45:47 -0300

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./view/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        comicneue: ['Comic Neue', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
