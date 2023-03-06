dc92f8b9008999dd260072894bf909c946e18d54
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-03-06 20:11:51 -0300

    swipers

--- js/app.js
+++ js/app.js
@@ -1,9 +1,77 @@
 var swiper = new Swiper(".tagsSwiper", {
 	slidesPerView: 'auto',
 	spaceBetween: 10,
-  freeMode: true,
+	freeMode: true,
+	pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+
+var swiper = new Swiper(".fotosSwiper", {
+	effect: "flip",
+	grabCursor: true,
 	pagination: {
 	  el: ".swiper-pagination",
-	  clickable: true,
+	},
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+  });
+
+var swiper = new Swiper(".tagsSwiper2", {
+	slidesPerView: 'auto',
+	spaceBetween: 10,
+	freeMode: true,
+	pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+	navigation: {
+		nextEl: ".swiper-button-next2",
+		prevEl: ".swiper-button-prev2",
+	},
+});
+
+var swiper = new Swiper(".fotosSwiper2", {
+	effect: "flip",
+	grabCursor: true,
+	pagination: {
+	  el: ".swiper-pagination",
+	},
+	navigation: {
+	  nextEl: ".swiper-button-next2",
+	  prevEl: ".swiper-button-prev2",
+	},
+  });
+
+var swiper = new Swiper(".tagsSwiper3", {
+	slidesPerView: 'auto',
+	spaceBetween: 10,
+	freeMode: true,
+	pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+	navigation: {
+		nextEl: ".swiper-button-next3",
+		prevEl: ".swiper-button-prev3",
+	},
+});
+
+var swiper = new Swiper(".fotosSwiper3", {
+	effect: "flip",
+	grabCursor: true,
+	pagination: {
+	  el: ".swiper-pagination",
+	},
+	navigation: {
+	  nextEl: ".swiper-button-next3",
+	  prevEl: ".swiper-button-prev3",
 	},
   });
