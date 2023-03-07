ec39b9192c24a81908eab02915515e9b2f084c56
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-03-07 20:22:07 -0300

    paginação fotos

--- js/app.js
+++ js/app.js
@@ -2,10 +2,6 @@ var swiper = new Swiper(".tagsSwiper", {
 	slidesPerView: 'auto',
 	spaceBetween: 10,
 	freeMode: true,
-	pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-      },
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
@@ -17,6 +13,7 @@ var swiper = new Swiper(".fotosSwiper", {
 	grabCursor: true,
 	pagination: {
 	  el: ".swiper-pagination",
+	  type: "fraction",
 	},
 	navigation: {
 	  nextEl: ".swiper-button-next",
@@ -28,10 +25,6 @@ var swiper = new Swiper(".tagsSwiper2", {
 	slidesPerView: 'auto',
 	spaceBetween: 10,
 	freeMode: true,
-	pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-      },
 	navigation: {
 		nextEl: ".swiper-button-next2",
 		prevEl: ".swiper-button-prev2",
@@ -43,6 +36,7 @@ var swiper = new Swiper(".fotosSwiper2", {
 	grabCursor: true,
 	pagination: {
 	  el: ".swiper-pagination",
+	  type: "fraction",
 	},
 	navigation: {
 	  nextEl: ".swiper-button-next2",
@@ -54,10 +48,6 @@ var swiper = new Swiper(".tagsSwiper3", {
 	slidesPerView: 'auto',
 	spaceBetween: 10,
 	freeMode: true,
-	pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-      },
 	navigation: {
 		nextEl: ".swiper-button-next3",
 		prevEl: ".swiper-button-prev3",
@@ -69,6 +59,7 @@ var swiper = new Swiper(".fotosSwiper3", {
 	grabCursor: true,
 	pagination: {
 	  el: ".swiper-pagination",
+	  type: "fraction",
 	},
 	navigation: {
 	  nextEl: ".swiper-button-next3",
