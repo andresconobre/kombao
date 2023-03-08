13174b964a9101d95c1b2af753207c878c7d5e37
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-03-07 21:59:11 -0300

    cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./view/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        red: {
+          amd: '#ED1C24',
+          mm: '#C1172B'
+        }
+      },
       fontFamily: {
         comicneue: ['Comic Neue', 'cursive'],
         rampart: ['Rampart One', 'cursive']
