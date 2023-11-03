361d6d3690e2610976d7f89a5ee6a778d9fc2821
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-03 13:53:20 -0300

    BannerHome

---
+++ src/components/BannerHome.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+import { ref } from "vue";
+import { frases } from '../composables/index'
+    
+    const randomIndex: number = Math.floor(Math.random() * frases.value.length);
+    const frase: string = frases.value[randomIndex];
+    
+</script>
+
+<template>
+    <div class="grid place-items-center h-screen font-space-mono">
+        <div class="container text-center">
+            <h1 class="inline-block text-flicker-in-glow text-7xl font-bold">KOMBÃO</h1>
+            <h2 class="mx-auto w-full md:w-2/3 pt-6 focus-in-contract-bck text-2xl">{{frase}}</h2>
+        </div>
+    </div>
+</template>
