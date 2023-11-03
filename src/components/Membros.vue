8274dc7babe3ab410de21b4fb6369056cc4ec57d
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-03 18:35:32 -0300

    Implementação Membros

---
+++ src/components/Membros.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { membros } from '../composables/index'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+</script>
+
+<template>
+    <div class="pt-header font-space-mono">
+        <div class="container py-12">
+            <h2 class="text-center font-bold text-4xl text-black pb-8">Membros</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
+                <div v-for="({ nome, lowername, fotos, tags }, index) in membros" :key="index" class="">
+                    <RouterLink :to="`/membros/${lowername}`">
+                        <swiper :slides-per-view="1" class="rounded-md group border-2 border-black relative hover:scale-105 duration-200">
+                            <swiper :breakpoints="{ 0:{ slidesPerView:1.5 }, 768:{ slidesPerView:2 } }" :space-between="20" loop class="!absolute w-full top-0 !px-4 mt-2 !opacity-0 group-hover:!opacity-100 transition duration-700">
+                                <swiper-slide v-for="tag in tags" :key="tag">
+                                    <p class="text-center bg-white rounded-lg font-raleway text-base border border-black">{{tag}}</p>
+                                </swiper-slide>
+                            </swiper>
+
+                            <swiper-slide v-for="foto in fotos" :key="foto">
+                                <img :src="`/img/${lowername}/${lowername}${foto}.png`" :alt="nome" class="object-cover aspect-[3/4] h-full w-full transition-transform duration-300 ease-in-out group-hover:-translate-y-10 z-10 relative hover:cursor-grab">
+                            </swiper-slide>
+                            <p class="absolute bottom-2 text-black text-center w-full">{{nome}} <img v-if="lowername === 'nicolas'" src="/img/boneco.png" alt="Boneco" class="h-4 absolute right-1 top-1/2 -translate-y-1/2"></p>
+                        </swiper>
+                    </RouterLink>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
--- src/components/MembrosHome.vue
+++ src/components/MembrosHome.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { membros } from '../composables/index'
 
 </script>
@@ -11,7 +10,8 @@ import { membros } from '../composables/index'
             <div class="relative flex justify-center items-center px-8 w-5/6 m-auto mt-16">
                 <div v-for="({ nome, lowername }, index) in membros" :key="index" class="-ml-6 md:-ml-8 hover:-translate-x-2 md:hover:-translate-x-4 transform transition group">
                     <RouterLink :to="`/membros/${lowername}`">
-                        <img class="w-14 sm:w-20 md:w-24 lg:w-28 rounded-full max-w-none shadow-md aspect-square object-cover group-hover:border-black border-white border-2 group-hover:scale-110" :src="`/img/${lowername}/${lowername}1.png`" :alt="nome">
+                        <img class="w-14 sm:w-20 md:w-24 lg:w-28 rounded-full max-w-none shadow-md aspect-square object-cover group-hover:border-black border-white border-2 group-hover:scale-110 z-10 relative" :src="`/img/${lowername}/${lowername}1.png`" :alt="nome">
+                        <img v-if="lowername === 'nicolas'" src="/img/boneco.png" alt="Boneco" class="absolute top-0 right-3 h-6 z-0 rotate-45">
                         <p class="relative text-center invisible group-hover:visible font-medium text-lg capitalize mt-4 bg-black rounded-md text-white tooltip-arrow">{{nome}}</p>
                     </RouterLink>
                 </div>
