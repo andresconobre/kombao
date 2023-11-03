f2496258a73398c1b555dfc88cefaff5a42e1ace
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-03 11:21:38 -0300

    Alteração Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { pages } from '../composables/index'
-import Home from "../views/Home.vue";
+import HomePage from "../components/HomePage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: Home,
+      component: HomePage,
     },
     // {
     //   path: "/about",
