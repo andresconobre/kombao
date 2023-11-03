c3740e9cab924a006dbd793b0f2a1228a48e32d1
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-03 13:54:20 -0300

    Alteração Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
     import { RouterLink } from "vue-router";
+    import Logo from './icons/LogoKombao.vue'
     const props = defineProps(['pages'])
 </script>
 
 <template>
-    <header class="bg-red-950 text-white fixed top-0 w-full">
-        <div class="container py-6 flex items-center justify-between">
+    <header class="py-6 z-50 bg-transparent shadow-[0_4px_30px_rgba(0,_0,_0,_0.1)] backdrop-blur-[7.2px] font-raleway font-semibold text-lg text-black fixed top-0 w-full">
+        <div class="container flex items-center justify-between">
             <RouterLink to="/">
-                Kombão
+                <Logo />
             </RouterLink>
             <ul class="flex items-center justify-between gap-8">
                 <li v-for="(page, index) in pages" :key="index">
