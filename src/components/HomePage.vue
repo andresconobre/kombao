f7894520125980e32d802480b1d8b269e3e5be7d
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-03 13:54:07 -0300

    Alteração HomePage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,17 +1,9 @@
 <script setup lang='ts'>
-import { ref } from "vue";
-import { frases } from '../composables/index'
-    
-    const randomIndex: number = Math.floor(Math.random() * frases.value.length);
-    const frase: string = frases.value[randomIndex];
-    
+import Banner from './BannerHome.vue'
+import MembrosHome from './MembrosHome.vue'
 </script>
 
 <template>
-    <div class="grid place-items-center h-screen font-space-mono">
-        <div class="container text-center">
-            <h1 class="inline-block text-flicker-in-glow text-7xl font-bold">KOMBÃO</h1>
-            <h2 class="mx-auto w-full md:w-2/3 pt-6 focus-in-contract-bck text-2xl">{{frase}}</h2>
-        </div>
-    </div>
+    <Banner />
+    <MembrosHome />
 </template>
