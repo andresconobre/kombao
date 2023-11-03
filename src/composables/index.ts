8cf859976e37217767ba8b9a324c047b4c64f078
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-03 11:21:45 -0300

    Frases

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,10 +1,29 @@
 import { ref } from "vue";
 
 const pages = ref<string[]>([
-    'membros', 
-    'galeria',
-  ])
+  'membros', 
+  'galeria',
+])
+
+const frases = ref<string[]>([
+  'Academia é um lugar para dois tipos de pessoas: os que não precisam e os que não têm mais jeito',
+  'Phantom 👽',
+  'Close hands 🤝',
+  'Ai que saudade da minha ex',
+  'O Nicolas é a Arca de Noé da gastronomia: já comeu dois de cada.',
+  'Sanduíche de oggs',
+  'Deus é maravilhoso',
+  'Budansly?',
+  'Horas iguais',
+  'Mapinha novo de Terraria?',
+  '100zinho no Keydrop?',
+  'bd',
+  'Ué',
+  'Quem perdoa é município',
+  'Bagaçou'
+])
 
 export {
-    pages
+    pages,
+    frases
 }
