8d461b2bb620574c36db123ac7101d53db078cdf
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-03 11:24:01 -0300

    Alterações RouterLink e RouterView

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-    import { RouterLink, RouterView } from "vue-router";
+    import { RouterLink } from "vue-router";
     const props = defineProps(['pages'])
 </script>
 
 <template>
-    <header class="bg-red-950 text-white">
-        <div class="container py-3 flex items-center justify-between">
-            <h1>Kombão</h1>
+    <header class="bg-red-950 text-white fixed top-0 w-full">
+        <div class="container py-6 flex items-center justify-between">
+            <RouterLink to="/">
+                Kombão
+            </RouterLink>
             <ul class="flex items-center justify-between gap-8">
                 <li v-for="(page, index) in pages" :key="index">
-                    <!-- <a href="" class="uppercase">{{page}}</a> -->
-                    <RouterLink :to="page" class="uppercase">{{page}}</RouterLink>
+                    <RouterLink :to="page" class="uppercase hover-underline-animation">{{page}}</RouterLink>
                 </li>
             </ul>
         </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import Header from '../components/Header.vue';
+import HomePage from '../components/HomePage.vue';
 import { pages } from '../composables/index'
 
 </script>
@@ -10,7 +11,7 @@ import { pages } from '../composables/index'
 
     <!-- <RouterLink to="/">Home</RouterLink> -->
     <Header :pages="pages"/>
+    <RouterView />
     
-  <!-- <RouterView /> -->
 </template>
 
