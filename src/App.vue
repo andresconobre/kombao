5390cd201c616bc0d19a1e825f5696380b597a43
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-02 22:48:48 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+
+</script>
+
+<template>
+
+    <!-- <RouterLink to="/">Home</RouterLink> -->
+    <div class="grid place-items-center bg-red-500 h-screen">
+      KOMBAO
+    </div>
+
+  <!-- <RouterView /> -->
+</template>
+
