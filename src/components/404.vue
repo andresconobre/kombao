efc9db9366d1ca320bba1df590ebceeed1845a29
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-04 01:19:25 -0300

    Criação página 404

---
+++ src/components/404.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="grid place-items-center h-screen font-space-mono text-center text-black">
+        <div class="container">
+            <h2 class="font-bold text-2xl md:text-6xl typing uppercase mx-auto !leading-[1.3]">Em construção</h2>
+            <h3 class="font-semibold text-xl md:text-3xl text-flicker-in-glow pt-4">Aguarde...</h3>
+            <img src="/img/boneco.png" alt="Boneco" class="object-contain h-32 mx-auto mt-12">
+        </div>
+    </div>
+</template>
