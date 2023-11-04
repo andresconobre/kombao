173380adcae7b3e117bfc605d9fdddd984821109
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-04 00:46:34 -0300

    Alterações MembrosHome

--- src/components/MembrosHome.vue
+++ src/components/MembrosHome.vue
@@ -9,11 +9,11 @@ import { membros } from '../composables/index'
             <h2 class="text-center font-bold text-3xl text-black">Membros</h2>
             <div class="relative flex justify-center items-center px-8 w-5/6 m-auto mt-16">
                 <div v-for="({ nome, lowername }, index) in membros" :key="index" class="-ml-6 md:-ml-8 hover:-translate-x-2 md:hover:-translate-x-4 transform transition group">
-                    <RouterLink :to="`/membros/${lowername}`">
-                        <img class="w-14 sm:w-20 md:w-24 lg:w-28 rounded-full max-w-none shadow-md aspect-square object-cover group-hover:border-black border-white border-2 group-hover:scale-110 z-10 relative" :src="`/img/${lowername}/${lowername}1.png`" :alt="nome">
+                    <!-- <RouterLink :to="`/membros/${lowername}`"> -->
+                        <img class="w-20 md:w-24 lg:w-28 rounded-full max-w-none shadow-md aspect-square object-cover group-hover:border-black border-white border-2 group-hover:scale-110 z-10 relative" :src="`/img/${lowername}/${lowername}1.png`" :alt="nome">
                         <img v-if="lowername === 'nicolas'" src="/img/boneco.png" alt="Boneco" class="absolute top-0 right-3 h-6 z-0 rotate-45">
-                        <p class="relative text-center invisible group-hover:visible font-medium text-lg capitalize mt-4 bg-black rounded-md text-white tooltip-arrow">{{nome}}</p>
-                    </RouterLink>
+                        <p class="relative tooltip tooltip-arrow">{{nome}}</p>
+                    <!-- </RouterLink> -->
                 </div>
             </div>
         </div>
