286a3bf76197bbcb9416b2f9f90f0c0fec326d80
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-09 08:10:10 -0300

    WIP Responsividade MembrosHome

--- src/components/MembrosHome.vue
+++ src/components/MembrosHome.vue
@@ -7,7 +7,7 @@ import { membros } from '../composables/index'
     <div class="grid place-items-center h-screen font-space-mono">
         <div class="container">
             <h2 class="text-center font-bold text-3xl text-black">Membros</h2>
-            <div class="relative flex justify-center items-center px-8 w-5/6 m-auto mt-16">
+            <div class="relative flex justify-center items-center px-8 w-5/6 m-auto mt-16" :class="{'flex-wrap': membros.length > 13}">
                 <div v-for="({ nome, lowername }, index) in membros" :key="index" class="-ml-6 md:-ml-8 hover:-translate-x-2 md:hover:-translate-x-4 transform transition group">
                     <!-- <RouterLink :to="`/membros/${lowername}`"> -->
                         <img class="w-20 md:w-24 lg:w-28 rounded-full max-w-none shadow-md aspect-square object-cover group-hover:border-black border-white border-2 group-hover:scale-110 z-10 relative" :src="`/img/${lowername}/${lowername}1.png`" :alt="nome">
