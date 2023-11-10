7f6c8557869be55a4d87f38a51e47847c7fda810
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-10 10:53:47 -0300

    Tailwind darkMode e cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  darkMode: 'class',
   theme: {
     fontFamily: {
       'space-mono': ['Space Mono', 'monospace'],
@@ -13,6 +14,15 @@ module.exports = {
     extend: {
       padding: {
         'header': '112px'
+      },
+      colors: {
+        gray: {
+          'dark': '#2b2b2b',
+          'light': '#eee',
+          'c1': '#383838',
+          'c2': '#2e2e2e',
+          'c3': '#272727',
+        }
       }
     },
   },
