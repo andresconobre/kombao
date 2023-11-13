bb4fc56a73511bca37c5725218953f620e704258
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-13 09:58:43 -0300

    Ajustes responsividade

--- src/components/BannerHome.vue
+++ src/components/BannerHome.vue
@@ -10,7 +10,7 @@ import { frases } from '../composables/index'
 <template>
     <div class="grid place-items-center h-screen font-space-mono">
         <div class="container text-center">
-            <h1 class="inline-block text-flicker-in-glow text-7xl font-bold dark:text-gray-light text-gray-dark">KOMBÃO</h1>
+            <h1 class="inline-block text-flicker-in-glow text-6xl md:text-7xl font-bold dark:text-gray-light text-gray-dark">KOMBÃO</h1>
             <h2 class="mx-auto w-full md:w-2/3 pt-6 focus-in-contract-bck text-2xl dark:text-gray-light text-gray-dark">{{frase}}</h2>
         </div>
     </div>
--- src/components/MembrosHome.vue
+++ src/components/MembrosHome.vue
@@ -7,10 +7,10 @@ import { membros } from '../composables/index'
     <div class="grid place-items-center h-screen font-space-mono">
         <div class="container">
             <h2 class="text-center font-bold text-3xl dark:text-gray-light text-gray-dark">Membros</h2>
-            <div class="relative flex justify-center items-center px-8 w-5/6 m-auto mt-16" :class="{'flex-wrap': membros.length > 13}">
-                <div v-for="({ nome, lowername }, index) in membros" :key="index" class="-ml-6 md:-ml-8 hover:-translate-x-2 md:hover:-translate-x-4 transform transition group">
+            <div class="relative md:flex justify-center items-center lg:px-8 lg:w-5/6 m-auto mt-16 flex-wrap grid grid-cols-2 place-items-center">
+                <div v-for="({ nome, lowername }, index) in membros" :key="index" class="md:-ml-8 md:hover:-translate-x-4 transform transition group grid md:block place-items-center pb-4 md:pb-0">
                     <!-- <RouterLink :to="`/membros/${lowername}`"> -->
-                        <img class="w-20 md:w-24 lg:w-28 rounded-full max-w-none shadow-md aspect-square object-cover dark:group-hover:border-gray-light group-hover:border-gray-dark dark:border-black border-white border-2 group-hover:scale-110 z-10 relative" :src="`/img/${lowername}/${lowername}1.png`" :alt="nome">
+                        <img class="w-24 lg:w-28 rounded-full max-w-none shadow-md aspect-square object-cover md:dark:group-hover:border-gray-light md:group-hover:border-gray-dark dark:border-white border-black border-2 md:group-hover:scale-110 z-10 relative" :src="`/img/${lowername}/${lowername}1.png`" :alt="nome">
                         <img v-if="lowername === 'nicolas'" src="/img/boneco.png" alt="Boneco" class="absolute top-0 right-3 h-6 z-0 rotate-45">
                         <p class="relative tooltip tooltip-arrow">{{nome}}</p>
                     <!-- </RouterLink> -->
