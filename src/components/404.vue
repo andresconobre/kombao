1a3be391fb1195d6a34b5d4cb956d07be6550fc0
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-11-13 09:14:21 -0300

    Ajuste darkmode 404

--- src/components/404.vue
+++ src/components/404.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="grid place-items-center h-screen font-space-mono text-center text-black">
+    <div class="grid place-items-center h-screen font-space-mono text-center dark:text-gray-light text-gray-dark">
         <div class="container">
             <h2 class="font-bold text-2xl md:text-6xl typing uppercase mx-auto !leading-[1.3]">Em construção</h2>
             <h3 class="font-semibold text-xl md:text-3xl text-flicker-in-glow pt-4">Aguarde...</h3>
